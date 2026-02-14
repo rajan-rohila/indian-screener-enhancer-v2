33af909059da84dcd4c8bccc2e5ddca1e103900c
Rajan Singh <rajasing@amazon.com>
2026-02-14 11:22:56 -0800

    Adding tabs

--- src/pages/Screener/Screener.tsx
+++ src/pages/Screener/Screener.tsx
@@ -1,7 +1,9 @@
-import { useState } from "react";
-import { SideNavigation, Header, Container } from "@cloudscape-design/components";
+import { useState, useMemo } from "react";
+import { SideNavigation, Header, Container, Tabs } from "@cloudscape-design/components";
 import type { SideNavigationProps } from "@cloudscape-design/components";
 import { INDUSTRY_GROUPS, type IndustryGroupKey } from "../../constants/industryGroups";
+import { INDUSTRIES, type IndustryKey } from "../../constants/industries";
+import { INDUSTRY_TREE } from "../../constants/industryTree";
 import { SIDEBAR_SECTIONS } from "./sidebarSections";
 
 const navItems: SideNavigationProps.Item[] = SIDEBAR_SECTIONS.flatMap((section, i) => {
@@ -20,6 +22,24 @@ export default function Screener() {
   const [activeHref, setActiveHref] = useState("#FINANCIAL");
   const activeKey = activeHref.replace("#", "") as IndustryGroupKey;
 
+  const industryKeys = useMemo(
+    () => Object.keys(INDUSTRY_TREE[activeKey]) as IndustryKey[],
+    [activeKey]
+  );
+
+  const [selectedIndustry, setSelectedIndustry] = useState<string>(industryKeys[0]);
+
+  // Reset selected industry when group changes
+  useMemo(() => {
+    setSelectedIndustry(industryKeys[0]);
+  }, [industryKeys]);
+
+  const tabItems = industryKeys.map((key) => ({
+    id: key,
+    label: INDUSTRIES[key],
+    content: <p>{INDUSTRIES[key]} content will appear here.</p>,
+  }));
+
   return (
     <div style={{ display: "flex", height: "100%", marginTop: "16px" }}>
       <div style={{ width: 250, flexShrink: 0 }}>
@@ -36,7 +56,11 @@ export default function Screener() {
       </div>
       <div style={{ flex: 1, paddingLeft: "20px", paddingRight: "20px" }}>
         <Container header={<Header variant="h1">{INDUSTRY_GROUPS[activeKey]}</Header>}>
-          <p>Screener content for {INDUSTRY_GROUPS[activeKey]} will appear here.</p>
+          <Tabs
+            activeTabId={selectedIndustry}
+            onChange={({ detail }) => setSelectedIndustry(detail.activeTabId)}
+            tabs={tabItems}
+          />
         </Container>
       </div>
     </div>
